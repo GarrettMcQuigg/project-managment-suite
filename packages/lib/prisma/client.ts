ce934ffdf88f48f7179c7b028eca7636d9271e04
Garrett <garrettmcquigg@gmail.com>
2025-01-16 18:12:50 -0600

    added database

--- packages/lib/prisma/client.ts
+++ packages/lib/prisma/client.ts
@@ -1,7 +1,5 @@
-// utils/supabase.ts
 import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://vrhdnvrozkrkeuesbirs.supabase.co';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const dbUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const dbKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const db = createClient(dbUrl, dbKey);
+const supabase = createClient(supabaseUrl, supabaseKey);

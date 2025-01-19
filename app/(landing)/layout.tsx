55e6769cb7f30a5e9e98da33f20771bdea146771
Garrett <garrettmcquigg@gmail.com>
2025-01-19 11:57:22 -0600

    build fix

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,9 +1,9 @@
-import { getCurrentUser } from '@/packages/lib/helpers/get-current-user';
+// import { getCurrentUser } from '@/packages/lib/helpers/get-current-user';
 // import Header from './_src/header';
 // import Footer from './_src/footer';
 
 export default async function HomeLayout({ children }: { children: React.ReactNode }) {
-  const currentUser = await getCurrentUser();
+  // const currentUser = await getCurrentUser();
 
   return (
     <>

86dfbd155d9a2bb5f7f0a9a6cbca5c1fdd6de1fb
Garrett <garrettmcquigg@gmail.com>
2025-02-02 09:27:51 -0600

    build fix

--- app/(landing)/_src/temp-landing-page.tsx
+++ app/(landing)/_src/temp-landing-page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Image from 'next/image';
+// import Image from 'next/image';
 import Link from 'next/link';
 import { Zap, Users, Calendar } from 'lucide-react';
 import CustomBackground from '@/packages/lib/components/custom-background';
